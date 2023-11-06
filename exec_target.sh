2a78978c9f453101374b0136e1893c5990dd86f8
Arnaud FABRE <arnaud-aa.fabre@laposte.net>
2023-11-06 14:19:51 +0100

    txt folder

--- exec_target.sh
+++ exec_target.sh
@@ -2,5 +2,6 @@ docker run -it --rm --name ransomware \
     --net=ransomware-network \
     -v "$PWD"/sources:/root/ransomware:ro \
     -v "$PWD"/dist:/root/bin:ro \
+    -v "$PWD"/txt_folder:/root/myTxtfiles:rw \
     ransomware \
-    /bin/bash -c "python3 /root/ransomware/create_txt_files.py && /bin/bash"
+    /bin/bash
